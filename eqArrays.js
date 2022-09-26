7ca20c88abe24bacd79452b21b948d104a021372
Kevin Lee <jhssttj@hotmail.com>
2022-09-26 13:54:57 -0600

    Export module for test cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,6 @@
-//This function will determine if the two input are identical and return true or false
-const assertEqual = function(actual, expected) {
-  let finalPass = '';
-  if (actual === expected) {
-    finalPass = (`😀😀😀Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    finalPass = (`🤬🤬🤬Assertion Failed: ${actual} !== ${expected}`);
-  }
-  console.log(finalPass);
-  return finalPass;
-};
+//Call in assertEqual function
+const assertEqual = require('./assertEqual');
+
 //The function below will compare each element in the two arrays and see if they are identical or not
 function eqArrays(array1, array2) {
   let output = true;
@@ -23,14 +15,5 @@ function eqArrays(array1, array2) {
   return output;
 };
 
-
-
-//Test case
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3,[4,5,6,[7,8,[9,10]]]], [1, 2, 3,[4,5,6,[7,8,[9,10]]]]), true);
-assertEqual(eqArrays([1, 2, 3,[4,5,6,[7,8,[9,10]]]], [1, 2, 3,4,5,6,7,8,9,10]), false);
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
-console.log(eqArrays([1,2,34], [1,2,[3,4]])) // => false
-
+module.exports = eqArrays;
 
--- tail.js
+++ tail.js
@@ -1,3 +1,6 @@
+//Call in assertEqual function
+const assertEqual = require('./assertEqual');
+
 //The function below will produce a new array with the first element removed
 const tail = function(element) {
   let finalArray = [];
@@ -8,20 +11,5 @@ const tail = function(element) {
   }
   return finalArray;
 };
-//This function will determine if the two input are identical and return true or false
-const assertEqual = function(actual, expected) {
-  let finalPass = '';
-  if (actual === expected) {
-    finalPass = (`😀😀😀Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    finalPass = (`🤬🤬🤬Assertion Failed: ${actual} !== ${expected}`);
-  }
-  console.log(finalPass);
-  return finalPass;
-};
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
